b1c80409b862766affb315bfd2086a95b1e8705d
Nick <nickwu0301@gmail.com>
2022-08-13 15:16:58 +0800

    update member validation feature

--- js/member_signup.js
+++ js/member_signup.js
@@ -5,6 +5,30 @@ window.onload = (e) => {
             window.location.replace("index.html");
         }
     };
+    document.getElementById("btn_signup").addEventListener("click", () => {
+        const API = URL + MEMBER;
+        member["memID"] = document.getElementById("memID").value;
+        member["memPassword"] = document.getElementById("memPassword").value;
+        member["memEmail"] = document.getElementById("memEmail").value;
+        member["memName"] = document.getElementById("memName").value;
+        member["memBirth"] = document.getElementById("memBirth").value;
+        member["memCellPhone"] = document.getElementById("memCellPhone").value;
+        member["memAddress"] = document.getElementById("memAddress").value;
+        let memPasswordConfirm =
+            document.getElementById("memPasswordConfirm").value;
+        let validation = accountValidation(member, memPasswordConfirm);
+        if (validation !== "pass") {
+            document.getElementById("login-message").innerText = validation;
+        } else {
+            axios
+                .post(API + "/register", member)
+                .then((response) => {
+                    let msg = registerMsg(response.data.msg);
+                    document.getElementById("login-message").innerText = msg;
+                })
+                .catch((error) => console.log(error));
+        }
+    });
 };
 function registerMsg(msg) {
     if (msg === "Invalid member account") {
@@ -18,22 +42,27 @@ function registerMsg(msg) {
     }
 }
 
-function signUp() {
-    const API = URL + MEMBER;
-    member["memID"] = document.getElementById("memID").value;
-    member["memPassword"] = document.getElementById("memPassword").value;
-    member["memEmail"] = document.getElementById("memEmail").value;
-    member["memName"] = document.getElementById("memName").value;
-    member["memBirth"] = document.getElementById("memBirth").value;
-    member["memCellPhone"] = document.getElementById("memCellPhone").value;
-    member["memAddress"] = document.getElementById("memAddress").value;
-    console.log(member);
-    axios
-        .post(API + "/register", member)
-        .then((response) => {
-            console.log(response.data);
-            let msg = registerMsg(response.data.msg);
-            document.getElementById("login-message").innerText = msg;
-        })
-        .catch((error) => console.log(error));
+function signUp() {}
+function accountValidation(member, memPasswordConfirm) {
+    if (/[(a-zA-Z0-9)]{6,20}/.test(member.memID) === false) {
+        return "無效的帳號";
+    }
+    if (
+        member.memPassword == null ||
+        member.memPassword == "" ||
+        member.memPassword !== memPasswordConfirm
+    ) {
+        return "輸入密碼錯誤";
+    }
+    if (
+        /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/.test(
+            member.memEmail
+        ) === false
+    ) {
+        return "無效的信箱";
+    }
+    if (/[0-9]{10}/.test(member.memCellPhone) === false) {
+        return "手機號碼錯誤";
+    }
+    return "pass";
 }
